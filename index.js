1192d6b4931e0c318d78364aaeb02f61f98f6b09
leossnet <leossnet@gmail.com>
2020-06-08 17:47:57 +0500

    реорганизация корневого класса приложения

--- index.js
+++ index.js
@@ -2,19 +2,39 @@
  * Основной класс клиентского приложения
  */
 class App {
+    #root; 
+    #butPanel;
+    #editFormula;
     #table;
+
     /**
      * Конструктор клиентского приложения
-     * @param {String} tableName наименование css-класса таблицы
-     * @param {Number} rowCount число строк таблицы
-     * @param {Number} colCount число колонок таблицы
+     * @param {String} appName id корневого узла приложения
+     * @param {Object} param различные параметры приложенич
      */
-    constructor (tableName, rowCount, colCount){
-        this.#table = new Table (tableName, {rowCount: rowCount, colCount: colCount});
-        document.querySelector("."+tableName).append(this.#table);
+    constructor (appName, param) {
+        this.#root = document.querySelector("#"+appName);
+
+        this.appendElement(this.#butPanel, "label", appName+"Label", "Кнопки");
+        this.appendElement(this.#editFormula, "input", appName+"Input");
+
+        this.#table = new Table (appName, { rowCount: param.rowCount, colCount: param.colCount });
+        this.#root.append(this.#table);
         this.#table.focus();
     }
-    
- 
+
+    /**
+     * Добавление нового компонента в приложение 
+     * @param {Object} root узел приложения
+     * @param {String} elemName - имя элемента
+     * @param {String} elemId - id элемента
+     * @param {String} elemInner - содержимое элемента (опционально)
+     */
+    appendElement(root, elemName, elemId, elemInner) {
+        root = document.createElement(elemName);
+        root.id = elemId;
+        if ( elemInner ) root.innerHTML = elemInner;
+        this.#root.append (root);
+    }
 }
 
