0e17d1ad7b45fbc25d2608081e01b902d019cc4c
leossnet <leossnet@gmail.com>
2020-06-13 18:38:18 +0500

    добавлена конфигурация nginx при установке

--- install/install.sh
+++ install/install.sh
@@ -6,3 +6,12 @@ fi
 cd /htdocs/
 
 git clone https://github.com/leossnet/bizcalc.git
+
+#nginx
+#конфиг bizcalc.conf предполагает наличие ssl-сертификатов для сайта bizcalc.ru в папке /etc/nginx/ssl
+#если таких сертификатов нет, то их нужно там разместить до запуска скрипта установки.
+sudo apt install -y nginx
+sudo cp /htdocs/bizcalc/install/bizcalc.conf /etc/nginx/sites-available/bizcalc.conf
+sudo ln -s /etc/nginx/sites-available/bizcalc.conf /etc/nginx/sites-enabled/bizcalc.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo service nginx restart
