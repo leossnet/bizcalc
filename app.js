6fddc9900ca46e3b22ba0c8d28fe91d162869753
leossnet <leossnet@gmail.com>
2020-06-20 14:30:29 +0500

    доработка сервера node.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+/**
+ * Серверный модуль веб-сервера
+ */
 const http = require('http');
 const fs = require('fs');
 
-http.createServer(function(request, response) {
-    const filePath = request.urs.substr();
-    fs.readFile(filePath, function(error, data) {
+/**
+ * Инициализация и запуск сервера приложения в контексте node.js
+ */
+http.createServer((request, response) => {
+    const filePath = request.url.substr(1);
+    fs.readFile(filePath, (error, data) => {
         if ( error ) {
             response.statusCode = 404;
-            response.end("Файл не найден.");
+            response.end("File not found...");
         }
         else {
-            response.end(data);
+            fs.createReadStream(filePath).pipe(response);
         }
     });
-}).listen(3000, function() {
-    console.log("Server started at 3000");
+}).listen(80, function() {
+    console.log("Server started at 80");
 });
