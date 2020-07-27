575311ee8e6b7be3603966a598cd8283e9f5ebcb
leossnet <leossnet@gmail.com>
2020-07-28 03:36:00 +0500

    bc-55 правка теста плавающей разметки

--- test/js/flexlayout.js
+++ test/js/flexlayout.js
@@ -2,12 +2,11 @@
   * Места расположения компонентов в контейнере
   */
 const Space = {
-    TOP: "header",
-    BOTTOM: "footer",
-    LEFT: "nav",
-    RIGHT: "aside",
-    MAIN: "main",
-    ROW: "div"
+    TOP: "flex-header",
+    BOTTOM: "flex-footer",
+    LEFT: "flex-nav",
+    RIGHT: "flex-aside",
+    MAIN: "flex-main"
 };
 
 class FlexLayout extends HTMLDivElement {
@@ -18,31 +17,13 @@ class FlexLayout extends HTMLDivElement {
         super();
         this.#components = new Map();
         this.classList.add("flex-layout");
-        this.generateFlexLayout();
         root.append(this);
     }
 
-    generateFlexLayout() {
-        this.createSpace(this, Space.TOP);
-        this.createSpace(this, Space.ROW, [Space.LEFT, Space.MAIN, Space.RIGHT]);
-        this.createSpace(this, Space.BOTTOM);
-    }
-
-    createSpace(root, space, childNodes) {
-        let elem = document.createElement(space);
-        this.#components.set(space, elem);
-        elem.classList.add("flex-" + space);
-        if (childNodes) childNodes.forEach(childSpace => {
-            let child = document.createElement(childSpace);
-            this.#components.set(childSpace, child);
-            child.classList.add("flex-" + childSpace);
-            elem.append(child);
-        });
-        this.append(elem);
-    }
-
     add(elem, space) {
-        this.#components.get(space).append(elem);
+        elem.classList.add(space);
+        this.#components.set(space, elem);
+        this.append(elem);
     }
 
 }
