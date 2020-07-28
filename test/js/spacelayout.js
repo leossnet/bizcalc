48b28952f267b23704b75c29af26f26c0cfb86f6
leossnet <leossnet@gmail.com>
2020-07-28 21:47:38 +0500

    bc-55 доработка блочного компоновщика

--- test/js/spacelayout.js
+++ test/js/spacelayout.js
@@ -9,6 +9,11 @@ const Space = {
     MAIN: "space-main"
 };
 
+/**
+  * Класс, обеспечивающий размещение компонентов по краям экрана и в центре.
+  * Для верхнего и нижнего компонента нужно установить свойство min-height,
+  * а для левого и правого комопнента - min-width
+  */
 class SpaceLayout extends HTMLDivElement {
     #components;
     #params;
@@ -27,13 +32,12 @@ class SpaceLayout extends HTMLDivElement {
             navWidth: 0,
             asideWidth: 0
         };
-        root.append(this);
-
         window.addEventListener("resize", () => { 
             for (let space of this.#components.keys()) {
                 this.updateStyle(this.#components.get(space), space);
             }
         });
+        root.append(this);
     }
 
     /**
@@ -48,26 +52,56 @@ class SpaceLayout extends HTMLDivElement {
         this.updateStyle(elem, space);
     }
 
+    /**
+     * Удаление компонента из указанного места расположения компоновщика, сам объект не удаляется
+     * @param {Object} space - место расположения компонента в виде атрибута объекта Space
+     */
+    remove(space) {
+        let elem = this.#components.get(space);
+        this.#components.delete(space);
+        elem.remove();
+        this.updateStyle(elem, space);
+    }
+
  
+    /**
+     * Обновление внешнего вида компоновщика
+     * @param {HTMLElement} elem - обновляемый компонент
+     * @param {Object} space - место расположения компонента в виде атрибута объекта Space
+     */
     updateStyle(elem, space) {
         let style = getComputedStyle(elem);
         switch (space) {
             case Space.TOP:
-                this.#params.headerHeight = Number.parseFloat(style.height);
-                this.setAttribute("header-height", Number.parseFloat(style.height));
+                this.setParam(elem, "headerHeight", "header-height", style.height);
                 break;
             case Space.BOTTOM:
-                this.#params.footerHeight = Number.parseFloat(style.height);
-                this.setAttribute("footer-height", Number.parseFloat(style.height));
+                this.setParam(elem, "footerHeight", "footer-height", style.height);
                 break;
             case Space.LEFT:
-                this.#params.navWidth = Number.parseFloat(style.width);
-                this.setAttribute("nav-width", Number.parseFloat(style.width));
+                this.setParam(elem, "navWidth", "nav-width", style.width);
                 break;
             case Space.RIGHT:
-                this.#params.asideWidth = Number.parseFloat(style.width);
-                this.setAttribute("aside-width", Number.parseFloat(style.width));
+                this.setParam(elem, "asideWidth", "aside-width", style.width);
                 break;
+        }
+    }
+
+    /**
+     * Уставноление параметров элемента 
+     * @param {HTMLElement} elem - обновляемый компонент
+     * @param {String} paramName - имя параметра компоновщика
+     * @param {String} attrName - имя html-атрибута компоновщика
+     * @param {any} value - значение атрибута в еденицах измерения
+     */
+    setParam(elem, paramName, attrName, value) {
+        if (elem) {
+            this.#params[paramName] = value ? Number.parseFloat(value) :  0;
+            this.setAttribute(attrName, value ? Number.parseFloat(value) : 0 );        
+        }
+        else {
+            this.#params[paramName] = 0;
+            this.setAttribute(attrName, 0);        
         }
     }
 
